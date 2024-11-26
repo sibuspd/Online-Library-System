13a63d77ea409788155dc6331e1749753882bb46
sibuspd <sibuspd@gmail.com>
2024-11-26 16:48:44 +0530

    Error component updated

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,17 +1,24 @@
 import { useRouteError} from "react-router-dom"
+import "./Error.css"
+import errorImage from "../assets/errorImage.png";
+import { Link } from "react-router-dom";
 
 function Error(){
 
     const err = useRouteError();
     console.log(err);
     return(
-        <>
-            <h1>Oops..........!</h1>
-            <h4>Please enter a valid URL.</h4>
-            <p>{err.data}</p>
-            <h3>{err.status}</h3>
-            <h3>{err.statusText}</h3>
-        </>
+            <div className="error-container">
+                <img src={errorImage} className="error-image" height="250px" width="250px" />
+                <h1>Oops... Something Went Wrong!</h1>
+                <h2>We can't seem to find the page you're looking for.</h2>
+                <h4>Please check the URL and try again.</h4>
+                <div className="error-details">
+                    <p><strong>Error Code:</strong> {err.status}</p>
+                    <p><strong>Message:</strong> {err.statusText}</p>
+                </div> 
+                <Link to="/"><button id="redirect-button">Go Back Home</button></Link>
+            </div>
     )
 }
 
